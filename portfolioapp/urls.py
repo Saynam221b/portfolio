3ca59e4ccbdbce0815400a2f8f2ca1e24fe6cf36
Saynam221b <saynam1101@gmail.com>
2023-06-26 17:31:21 +0530

    commit

---
+++ portfolioapp/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from portfolioapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index', views.index, name= 'index'),
+    path('projects', views.projects, name= 'projects'),
+    path('experience', views.experience, name= 'experience'),
+    path('contact', views.contact, name= 'contact'),
+    path('contact_success', views.contact_me, name= 'contact'),
+    
+
+
+]
